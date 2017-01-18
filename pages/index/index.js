5d7bf050f679fc2822a0e25922d3d69549606f02
chengguojiang <chengguojiang@58ganji.com>
2017-01-18 19:47:55 +0800

    add

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,6 +3,31 @@
 var app = getApp()
 Page( {
   data: {
+
+    hotList: [
+      {
+        pic: 'https://m.youcai.xin/static/img/gravida.png',
+        title: '玻璃',
+        desc: '22W人去過'
+      }, {
+        pic: 'https://hamlet.b0.upaiyun.com/1609/19171/2788065f96c04ad38c8db50ad723bc37.jpg!/fwfh/640x352/quality/80',
+        title: '玻璃棧道',
+        desc: '22W人去過'
+      }, {
+        pic: 'https://hamlet.b0.upaiyun.com/1609/19171/2788065f96c04ad38c8db50ad723bc37.jpg!/fwfh/640x352/quality/80',
+        title: '玻璃棧道',
+        desc: '22W人去過'
+      }, {
+        pic: 'https://hamlet.b0.upaiyun.com/1609/19171/2788065f96c04ad38c8db50ad723bc37.jpg!/fwfh/640x352/quality/80',
+        title: '玻璃棧道',
+        desc: '22W人去過'
+      }, {
+        pic: 'https://hamlet.b0.upaiyun.com/1609/19171/2788065f96c04ad38c8db50ad723bc37.jpg!/fwfh/640x352/quality/80',
+        title: '玻璃棧道',
+        desc: '22W人去過'
+      }
+    ],
+
     // 轮播
     images: [
       'https://hamlet.b0.upaiyun.com/1609/19171/2788065f96c04ad38c8db50ad723bc37.jpg!/fwfh/640x352/quality/80',
